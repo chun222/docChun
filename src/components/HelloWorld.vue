d135a5401c64c40ad7b28edd7a2ea7a80c380b46
dcy <592985509@qq.com>
2022-08-09 14:47:00 +0800

    u

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
  * @gitee: https://gitee.com/chun22222222
  * @github: https://github.com/chun222
  * @Desc: 
- * @LastEditTime: 2022-08-08 18:08:01
+ * @LastEditTime: 2022-08-09 14:41:54
  * @FilePath: \web\src\components\HelloWorld.vue
 -->
 <script setup>
@@ -37,58 +37,120 @@ marked.setOptions({
   smartypants: false,
   xhtml: false,
 });
+console.log(`::tip
+ 
+abcdefgsssaaa
+::`.replace(/::tip/,"").replace(/::/,""));
 // 使用正则
-let reg = /(?<=(:tip))[\s\S]*?(?=:)/;
-let str = `:tip\n
-sss
-:`;
-str = `:tip
-ssss:`
-// 使用
-console.log(str.match(reg));
-str.match(reg); // 输出 ['1234']
+// let reg = /(:tip)/;
+// let str = `:tip
+// sss
+// :`;
+// // 使用
+// console.log(str.match(reg));
+// str.match(reg); // 输出 ['1234']
 
+// const descriptionList = {
+//   name: "descriptionList",
+//   level: "block", // Is this a block-level or inline-level tokenizer?
+//   start(src) {  
+//     return src.match(/:[^:\n]/)?.index;
+//   }, // Hint to Marked.js to stop and check for a match
+//   tokenizer(src, tokens) {
+//     // console.log(src,"===tokenizersrc");
+//     const rule = /^(?::tip:[^:\n]*(?:\n|$))+/; // /^(?:::tip[\n\s])[\s\S]*(?=::\n)/
+//     const match = rule.exec(src);
+//     // const match = src.match(rule);
+   
+//     if (match) {
+//        console.log(src, match, "match");
+//       const token = {
+//         // Token to generate
+//         type: "descriptionList", // Should match "name" above
+//         raw: match[0], // Text to consume from the source
+//         text: match[0].trim(), // Additional custom properties .replace(/::tip/,"")
+//         tokens: [], // Array where child inline tokens will be generated
+//       };
+//       this.lexer.inline(token.text, token.tokens); // Queue this data to be processed for inline tokens blockTokens
+//       return token;
+//     }
+//   },
+//   renderer(token) {
+//     return `<div class='tip' style="color:red">${this.parser.parseInline(token.tokens)}</div>`; // parseInline to turn child tokens into HTML
+//   },
+// };
+
+// const description = {
+//   name: 'description',
+//   level: 'inline',                                 // Is this a block-level or inline-level tokenizer?
+//   start(src) { return src.match(/:/)?.index; },    // Hint to Marked.js to stop and check for a match
+//   tokenizer(src, tokens) {
+//     const rule = /^:([^:\n]+):([^:\n]*)(?:\n|$)/;  // Regex for the complete token, anchor to string start
+//     const match = rule.exec(src);
+//     if (match) {
+//       return {                                         // Token to generate
+//         type: 'description',                           // Should match "name" above
+//         raw: match[0],                                 // Text to consume from the source
+//         dt: this.lexer.inlineTokens(match[1].trim()),  // Additional custom properties, including
+//         dd: this.lexer.inlineTokens(match[2].trim())   //   any further-nested inline tokens
+//       };
+//     }
+//   },
+//   renderer(token) {
+//       return `\n<div class='tip' style="color:red">${this.parser.parseInline(token.dd)}</div>`
+//     // return `\n<dt>${this.parser.parseInline(token.dt)}</dt><dd>${this.parser.parseInline(token.dd)}</dd>`;
+//   },
+//   childTokens: ['dt', 'dd'],                 // Any child tokens to be visited by walkTokens
+// };
 
 const descriptionList = {
-  name: "descriptionList",
-  level: "block", // Is this a block-level or inline-level tokenizer?
-  start(src) {
-    
-   // console.log(src.match(/:\n/)?.index); 
-    return src.match(/:\n/)?.index; 
-  }, // Hint to Marked.js to stop and check for a match
+  name: 'descriptionList',
+  level: 'block',                                     // Is this a block-level or inline-level tokenizer?
+  start(src) { 
+   // console.log( src.match(/:tip:\n/)?.index,"start",src);
+    return src.match(/:tip:/)?.index;
+     }, // Hint to Marked.js to stop and check for a match /:tip:[^:\n]/
   tokenizer(src, tokens) {
-   // console.log(src,"===src");
-    const rule = /(?<=(:tip\n)).*?(?=:)/; // Regex for the complete token, anchor to string start
-   //  const match = rule.exec(src);
-     const match = src.match(rule);
-  //  console.log(src,match,"match");
+    const rule = /^(?::tip:)[\S\s]*?(?<=::\n)/;    // Regex for the complete token, anchor to string start /^(?::[^:\n]+:[^:\n]*(?:\n|$))+/
+    const match = rule.exec(src);
+  console.log("match",match,src);
     if (match) {
-      const token = {
-        // Token to generate
-        type: "descriptionList", // Should match "name" above
-        raw: match[0], // Text to consume from the source
-        text: match[0].trim(), // Additional custom properties
-        tokens: [], // Array where child inline tokens will be generated
+      const token = {                                 // Token to generate
+        type: 'descriptionList',                      // Should match "name" above
+        raw: match[0],                                // Text to consume from the source
+        text: match[0].trim(),                        // Additional custom properties
+        tokens: []                                    // Array where child inline tokens will be generated
       };
-      this.lexer.inline(token.text, token.tokens); // Queue this data to be processed for inline tokens
+      this.lexer.inline(token.text, token.tokens); 
+      //this.lexer.inline(token.text, token.tokens);    // Queue this data to be processed for inline tokens  blockTokens
       return token;
     }
   },
   renderer(token) {
-    return `<div class='tip'>${this.parser.parseInline(token.tokens)}\n</div>`; // parseInline to turn child tokens into HTML
-  },
+    return `<div class='tip' style="color:red">${this.parser.parseInline(token.tokens)}</div>`; // parseInline to turn child tokens into HTML 
+  }
 };
+ 
+marked.use({ extensions: [descriptionList] }); 
 
-marked.use({ extensions: [descriptionList] });
+const rawHtml = marked.parse(`
+水水水水
+:tip:
+Topic 1:  Description 1   
+:Topic 1:  Description 1  
+::
 
-const rawHtml = marked.parse(`:tip
-sss
-:
-`);
+## 混合
 
+:tip:
+Topic 1:  Description 1   
+:Topic 1:  Description 1  
+::
 
-// ` 
+# 事实上
+啊`);
+
+// `
 // # Marked in Node.js
 // Rendered by **marked**.
 
